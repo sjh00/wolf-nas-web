1ee510416d1d10c6cc41a35b68c5ec72145f9e11
sjh00 <sjh00@outlook.com>
2026-09-14 19:39:42 +0800

    feat(deploy): 前端 nginx 支持 LISTEN_PORT

--- scripts/deploy/docker-entrypoint.sh
+++ scripts/deploy/docker-entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 
 BACKEND_HOST="${BACKEND_HOST:-wolfnas}"
 BACKEND_PORT="${BACKEND_PORT:-8080}"
+LISTEN_PORT="${LISTEN_PORT:-8080}"
 
-export BACKEND_HOST BACKEND_PORT
+export BACKEND_HOST BACKEND_PORT LISTEN_PORT
 
-envsubst '${BACKEND_HOST} ${BACKEND_PORT}' \
+envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${LISTEN_PORT}' \
     < /etc/nginx/nginx.conf.template \
     > /etc/nginx/nginx.conf
 
